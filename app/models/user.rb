dd5321e2dae8f2b740d2775941a467d78c45aa39
Chris Cotter <chris@Chriss-MacBook-Air.local>
2021-07-23 15:56:08 +1000

    made a few changes to players and users and some other stuff i guess

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts, dependent: :destroy
+    has_one :player, dependent: :destroy
 
     validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2021_07_23_022157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "players", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
@@ -54,8 +44,6 @@ ActiveRecord::Schema.define(version: 2021_07_23_022157) do
     t.boolean "admin"
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
   add_foreign_key "players", "users"
   add_foreign_key "posts", "users"
 end
