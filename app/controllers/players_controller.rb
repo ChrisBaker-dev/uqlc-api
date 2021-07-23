b8850cd0981e5856d5c43aafcd719d4ff69443cc
Christopher Baker <christopherbaker.dev@gmail.com>
2021-07-23 12:56:24 +1000

    Player controller complete

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,9 @@
 class PlayersController < ApplicationController
-    before_action :set_player, only: [:show]
+    before_action :set_player, only: [:show, :update, :destroy]
+
+    rescue_from Exception do |error|
+        render json: { error: error }, status: :not_found
+    end
 
     def index
         render json: Player.order(updated_at: :desc)
@@ -10,13 +14,27 @@ class PlayersController < ApplicationController
     end
 
     def create
-        render json: Player.create()
+        render json: Player.create(player_params), status: :created
+    end
+
+    def update
+        if @player.update(player_params)
+            render json: @player
+        else
+            render json: { error: "Unable to update player" }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if @player.destroy
+            render json: { message: "Player was deleted" }, status: :ok
+        else
+            render json: { error: "Unable to delete player" }, status: :unprocessable_entity
+        end
     end
 
 
 
-
-    
 
     private
 
