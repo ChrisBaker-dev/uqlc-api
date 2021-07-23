c12129242df95390fdf37ced9ee0113fe14fee29
Christopher Baker <christopherbaker.dev@gmail.com>
2021-07-23 16:08:57 +1000

    fixing cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://uqlc.netlify.app/'
+    origins 'https://uqlc.netlify.app'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
