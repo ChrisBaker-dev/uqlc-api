30079c9bae7f8ed0d9c9a27036ecb02f38f382eb
Christopher Baker <christopherbaker.dev@gmail.com>
2021-07-26 21:11:34 +1000

    testing complete | minitests

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2021_07_26_023344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "players", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
@@ -42,9 +52,13 @@ ActiveRecord::Schema.define(version: 2021_07_26_023344) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.boolean "admin"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "players", "users"
   add_foreign_key "posts", "users"
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "the truth" do
     assert true
   end
+  setup do
+    @user = User.create(email: "tyty@ty.com", password:"hello")
+    @user.save
+  end
 
   test "should not save user if username and email missing" do
     user = User.new() 
@@ -14,15 +18,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
     user = User.new(password: "hello")
     assert_not user.save
+  end
 
+  test "Should create a user" do
     user = User.new(email: "foo@bar.com", password: "hello")
     assert user.save
   end
 
-  
+  test "Login Path" do
+    post login_path, params: { email: @user.email, password: @user.password }
+    assert_response :ok
+  end
 
-
-
-  
+  test "register path" do
+    post register_path, params: {email: "yoink@whatt.com", password: "hello"}
+    assert_response :created
+  end
 
 end
